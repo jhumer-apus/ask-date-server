35b0d8d13dc08d827e746f3093ebb41a4c7e5a4c
jhumer-apus <jhumerapus@gmail.com>
2025-04-05 11:28:14 +0800

    updated

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,25 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/send-mail', function (Request $request) {
     $girl_name = $request["girl_name"];
-
     $message = $request['message'];
     $email = $request['email'];
 
-    $subject = "CASUAL DATE WITH ".$girl_name;
+    $subject = "CASUAL DATE WITH " . $girl_name;
 
-    Mail::to($email)->send(new CasualDateMail($subject, $message));
+    try {
+        // Attempt to send the email
+        Mail::to($email)->send(new CasualDateMail($subject, $message));
+
+        // If email is sent successfully, return a 200 response
+        return response()->json([
+            'message' => 'Email sent successfully!'
+        ], 200);
+        
+    } catch (\Exception $e) {
+        // In case of error, return a 500 error with a failure message
+        return response()->json([
+            'error' => 'Failed to send the email. Please try again later.',
+            'details' => $e->getMessage()
+        ], 500);
+    }
 });
